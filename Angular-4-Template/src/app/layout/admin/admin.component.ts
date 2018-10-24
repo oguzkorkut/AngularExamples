1439de763ec885faff1575b588625c5740c97202
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-24 21:12:07 +0300

    Ekran tasarımı yapıldı

--- Angular-4-Template/src/app/layout/admin/admin.component.ts
+++ Angular-4-Template/src/app/layout/admin/admin.component.ts
@@ -2,20 +2,12 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {animate, AUTO_STYLE, state, style, transition, trigger} from '@angular/animations';
 import {MenuItems} from '../../shared/menu-items/menu-items';
 
-/*
-import "../../../assets/js/particles.js"
-*/
-declare const $: any;
-
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: [
-    '../../../assets/css/styles.css',
-    '../../../assets/css/uikit.css'
   ]
 })
-
 
 export class AdminComponent implements OnInit {
 
--- Angular-4-Template/src/app/pages/dashboard/dashboard-default/dashboard-default.component.ts
+++ Angular-4-Template/src/app/pages/dashboard/dashboard-default/dashboard-default.component.ts
@@ -1,40 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
-/*
-import '../../../../assets/js/lightbox.min.js'
-import '../../../../assets/js/jquery.ajaxchimp.min.js'
-import '../../../../assets/js/js.js'
-import '../../../../assets/js/loading-bars.js'
-import '../../../../assets/js/ngTouch.min.js'
-import '../../../../assets/js/particles.min.js'
-import '../../../../assets/js/scripts.ang.js'
-import '../../../../assets/js/slider.min.js'
-import '../../../../assets/js/slideshow.min.js'
-import '../../../../assets/js/slideshow-fx.min.js'
-import '../../../../assets/js/uikit.min.js'
-*/ 
-
-declare const $: any;
 
 @Component({
   selector: 'app-dashboard-default',
   templateUrl: './dashboard-default.component.html',
   styleUrls: [
-    './dashboard-default.component.css',
-    '../../../../assets/css/styles.css',
-    '../../../../assets/css/uikit.css'
+    './dashboard-default.component.css'
   ]
 })
 export class DashboardDefaultComponent implements OnInit {
-
+/**
+  '../../../assets/css/styles.css',
+    '../../../assets/css/uikit.css'
+ */
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onTaskStatusChange(event) {
-    const parentNode = (event.target.parentNode.parentNode);
-    parentNode.classList.toggle('done-task');
-  }
+
 }
